8e4abb1808b973261c9a29b09e02cc3100951fb5
MistPhizzle <mistphizzle@gmail.com>
2013-07-30 19:58:09 -0400

    Silly commit
    I fixed something that wasn't even broken and spent way too long doing
    it.

--- src/com/etriacraft/probending/PlayerListener.java
+++ src/com/etriacraft/probending/PlayerListener.java
@@ -46,49 +46,51 @@ public class PlayerListener implements Listener {
 					if (Methods.getPlayerTeam(p.getName()).equalsIgnoreCase(Methods.TeamTwo))teamSide = Methods.TeamTwo; 
 				}
 
-
-
-				if (Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t2z3)&& !toRegions.contains(Methods.t2z3)) {
-					if (fromRegions.contains(Methods.t2z3)) {
-						Methods.allowedZone.remove(p.getName());
-						Methods.sendPBChat(Strings.PlayerEliminated.replace("%player", p.getName()));
-						p.getInventory().setArmorContents(null);
-						p.getInventory().setArmorContents(Commands.tmpArmor.get(p));
-						Commands.tmpArmor.remove(p);
-						if (Methods.playersInZone(Methods.t2z3).size() == 0) {
-							Methods.sendPBChat(Strings.RoundStopped);
-							Methods.sendPBChat(Strings.TeamWon.replace("%team", Methods.TeamOne));
-							Bukkit.getServer().getScheduler().cancelTask(Commands.clockTask);
-							Methods.matchStarted = false;
-							Methods.playingTeams.clear();
-							Methods.TeamOne = null;
-							Methods.TeamTwo = null;
-							Methods.allowedZone.clear();
-							Methods.restoreArmor();
+				if (Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t2z3)) {
+					if (!toRegions.contains(Methods.t2z3)|| toRegions.isEmpty()) {
+						if (fromRegions.contains(Methods.t2z3)) {
+							Methods.allowedZone.remove(p.getName());
+							Methods.sendPBChat(Strings.PlayerEliminated.replace("%player", p.getName()));
+							p.getInventory().setArmorContents(null);
+							p.getInventory().setArmorContents(Commands.tmpArmor.get(p));
+							Commands.tmpArmor.remove(p);
+							if (Methods.playersInZone(Methods.t2z3).size() == 0) {
+								Methods.sendPBChat(Strings.RoundStopped);
+								Methods.sendPBChat(Strings.TeamWon.replace("%team", Methods.TeamOne));
+								Bukkit.getServer().getScheduler().cancelTask(Commands.clockTask);
+								Methods.matchStarted = false;
+								Methods.playingTeams.clear();
+								Methods.TeamOne = null;
+								Methods.TeamTwo = null;
+								Methods.allowedZone.clear();
+								Methods.restoreArmor();
+							}
 						}
 					}
 					return;
 				}
-				if (Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t1z3) && !toRegions.contains(Methods.t1z3)) {
-					if (fromRegions.contains(Methods.t1z3)) {
-						Methods.allowedZone.remove(p.getName());
-						Methods.sendPBChat(Strings.PlayerEliminated.replace("%player", p.getName()));
-						p.getInventory().setArmorContents(null);
-						p.getInventory().setArmorContents(Commands.tmpArmor.get(p));
-						Commands.tmpArmor.remove(p);
-						if (Methods.playersInZone(Methods.t1z3).size() == 0) {
-							Methods.sendPBChat(Strings.RoundStopped);
-							Methods.sendPBChat(Strings.TeamWon.replace("%team", Methods.TeamTwo));
-							Bukkit.getServer().getScheduler().cancelTask(Commands.clockTask);
-							Methods.matchStarted = false;
-							Methods.playingTeams.clear();
-							Methods.TeamOne = null;
-							Methods.TeamTwo = null;
-							Methods.allowedZone.clear();
-							Methods.restoreArmor();
+				if (Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t1z3)) {
+					if (!toRegions.contains(Methods.t1z3)|| toRegions.isEmpty()) {
+						if (fromRegions.contains(Methods.t1z3)) {
+							Methods.allowedZone.remove(p.getName());
+							Methods.sendPBChat(Strings.PlayerEliminated.replace("%player", p.getName()));
+							p.getInventory().setArmorContents(null);
+							p.getInventory().setArmorContents(Commands.tmpArmor.get(p));
+							Commands.tmpArmor.remove(p);
+							if (Methods.playersInZone(Methods.t1z3).size() == 0) {
+								Methods.sendPBChat(Strings.RoundStopped);
+								Methods.sendPBChat(Strings.TeamWon.replace("%team", Methods.TeamTwo));
+								Bukkit.getServer().getScheduler().cancelTask(Commands.clockTask);
+								Methods.matchStarted = false;
+								Methods.playingTeams.clear();
+								Methods.TeamOne = null;
+								Methods.TeamTwo = null;
+								Methods.allowedZone.clear();
+								Methods.restoreArmor();
+							}
 						}
 					}
-					return;
+					return;                                                            
 				}
 				if (toRegions.contains(Methods.t2z3)) {
 					if (!Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t2z3)) {
@@ -114,7 +116,6 @@ public class PlayerListener implements Listener {
 				}
 				if (toRegions.contains(Methods.t1z3)) {
 					if (!Methods.allowedZone.containsKey(p.getName())) return;
-
 					if (!Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t1z3)) {
 						// Check Team One Zone 2
 						if (fromRegions.contains(Methods.t1z2) && Methods.allowedZone.get(p.getName()).equalsIgnoreCase(Methods.t1z2)) {
@@ -408,64 +409,64 @@ public class PlayerListener implements Listener {
 		}
 	} 
 
-@EventHandler
-public static void onPlayerJoin(PlayerJoinEvent e) {
-	Player player = e.getPlayer();
-	if (!(Tools.getBendingTypes(player).size() > 1)) {
-		String team = Methods.getPlayerTeam(player.getName());
-		if (team != null) {
-			String playerElement = null;
-			if (Tools.isBender(player.getName(), BendingType.Air)) {
-				playerElement = "Air";
-			}
-			if (Tools.isBender(player.getName(), BendingType.Water)) {
-				playerElement = "Water";
-			}
-			if (Tools.isBender(player.getName(), BendingType.Earth)) {
-				playerElement = "Earth";
-			}
-			if (Tools.isBender(player.getName(), BendingType.Fire)) {
-				playerElement = "Fire";
-			}
-			if (Tools.isBender(player.getName(), BendingType.ChiBlocker)) {
-				playerElement = "Chi";
-			}
-			String playerElementInTeam = Methods.getPlayerElementInTeam(player.getName(), team);
-			if (playerElementInTeam != null) {
-				if (!playerElementInTeam.equals(playerElement)) {
-					player.sendMessage(Strings.Prefix + Strings.RemovedFromTeamBecauseDifferentElement);
-					Methods.removePlayerFromTeam(team, player.getName(), playerElementInTeam);
-					Set<String> teamElements = Methods.getTeamElements(team);
-					if (teamElements.contains("Air")) {
-						String airbender = Methods.getTeamAirbender(team);
-						Methods.setOwner(airbender, team);
-						return;
-					}
-					if (teamElements.contains("Water")) {
-						String bender = Methods.getTeamWaterbender(team);
-						Methods.setOwner(bender, team);
-						return;
-					}
-					if (teamElements.contains("Earth")) {
-						String bender = Methods.getTeamEarthbender(team);
-						Methods.setOwner(bender, team);
-						return;
-					}
-					if (teamElements.contains("Fire")) {
-						String bender = Methods.getTeamFirebender(team);
-						Methods.setOwner(bender, team);
-						return;
-					}
-					if (teamElements.contains("Chi")) {
-						String bender = Methods.getTeamChiblocker(team);
-						Methods.setOwner(bender, team);
-						return;
-					} else {
-						Methods.deleteTeam(team);
+	@EventHandler
+	public static void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		if (!(Tools.getBendingTypes(player).size() > 1)) {
+			String team = Methods.getPlayerTeam(player.getName());
+			if (team != null) {
+				String playerElement = null;
+				if (Tools.isBender(player.getName(), BendingType.Air)) {
+					playerElement = "Air";
+				}
+				if (Tools.isBender(player.getName(), BendingType.Water)) {
+					playerElement = "Water";
+				}
+				if (Tools.isBender(player.getName(), BendingType.Earth)) {
+					playerElement = "Earth";
+				}
+				if (Tools.isBender(player.getName(), BendingType.Fire)) {
+					playerElement = "Fire";
+				}
+				if (Tools.isBender(player.getName(), BendingType.ChiBlocker)) {
+					playerElement = "Chi";
+				}
+				String playerElementInTeam = Methods.getPlayerElementInTeam(player.getName(), team);
+				if (playerElementInTeam != null) {
+					if (!playerElementInTeam.equals(playerElement)) {
+						player.sendMessage(Strings.Prefix + Strings.RemovedFromTeamBecauseDifferentElement);
+						Methods.removePlayerFromTeam(team, player.getName(), playerElementInTeam);
+						Set<String> teamElements = Methods.getTeamElements(team);
+						if (teamElements.contains("Air")) {
+							String airbender = Methods.getTeamAirbender(team);
+							Methods.setOwner(airbender, team);
+							return;
+						}
+						if (teamElements.contains("Water")) {
+							String bender = Methods.getTeamWaterbender(team);
+							Methods.setOwner(bender, team);
+							return;
+						}
+						if (teamElements.contains("Earth")) {
+							String bender = Methods.getTeamEarthbender(team);
+							Methods.setOwner(bender, team);
+							return;
+						}
+						if (teamElements.contains("Fire")) {
+							String bender = Methods.getTeamFirebender(team);
+							Methods.setOwner(bender, team);
+							return;
+						}
+						if (teamElements.contains("Chi")) {
+							String bender = Methods.getTeamChiblocker(team);
+							Methods.setOwner(bender, team);
+							return;
+						} else {
+							Methods.deleteTeam(team);
+						}
 					}
 				}
 			}
 		}
 	}
-}
 }
