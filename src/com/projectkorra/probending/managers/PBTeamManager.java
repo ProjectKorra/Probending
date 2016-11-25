7169a9239ccffe8d038993cb32ee065357fc1c73
Simplicitee <simplicitee123@gmail.com>
2016-11-25 01:25:46 -0500

    Revert "Revert "Finish teams frontend""
    This reverts commit 7539255bf2abf917b363d583f4b132416db1e1f7.

--- src/com/projectkorra/probending/managers/PBTeamManager.java
+++ src/com/projectkorra/probending/managers/PBTeamManager.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.projectkorra.probending.enums.TeamColor;
 import com.projectkorra.probending.libraries.database.Callback;
 import com.projectkorra.probending.objects.PBTeam;
 import com.projectkorra.probending.objects.PBTeam.TeamMemberRole;
@@ -21,6 +22,7 @@ public class PBTeamManager {
 	private Map<Player, PBTeam> teamsByOnlinePlayer = new HashMap<>();
 	private Map<Integer, PBTeam> teamsByID = new HashMap<>();
 	private Map<String, PBTeam> teamsByName = new HashMap<>();
+	private Map<TeamColor[], PBTeam> teamsByColor = new HashMap<>();
 
 	public PBTeamManager(DBProbendingTeam teamStorage) {
 		this.teamStorage = teamStorage;
@@ -37,6 +39,7 @@ public class PBTeamManager {
 				for (PBTeam team : teams) {
 					teamsByID.put(team.getID(), team);
 					teamsByName.put(team.getTeamName(), team);
+					teamsByColor.put(team.getColors(), team);
 					for (Player player : Bukkit.getOnlinePlayers()) {
 						if (team.getMembers().containsKey(player.getUniqueId())) {
 							teamsByOnlinePlayer.put(player, team);
@@ -85,6 +88,10 @@ public class PBTeamManager {
 		return teamsByName.containsKey(name) ? teamsByName.get(name) : null;
 	}
 	
+	public PBTeam getTeamFromColors(TeamColor[] colors) {
+		return teamsByColor.containsKey(colors) ? teamsByColor.get(colors) : null;
+	}
+	
 	public void createNewTeam(Player leader, String name, TeamMemberRole leaderRole, final Callback<Boolean> successCallback/*, Integer[] color*/) {
 		if (teamsByOnlinePlayer.containsKey(leader)) {
 			successCallback.run(false);
@@ -120,19 +127,20 @@ public class PBTeamManager {
 		}
 	}
 	
-	public void handleJoinTeam(Player player, final PBTeam team, TeamMemberRole role, final Callback<Boolean> successCallback) {
+	public boolean handleJoinTeam(Player player, final PBTeam team, TeamMemberRole role, final Callback<Boolean> successCallback) {
 		if (teamsByOnlinePlayer.containsKey(player)) {
 			successCallback.run(false);
+			return false;
 		} else {
 			BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
 			if (!bPlayer.hasElement(role.getElement())) {
 				successCallback.run(false);
-				return;
+				return false;
 			}
 			
 			if (!role.isEnabled()) {
 				successCallback.run(false);
-				return;
+				return false;
 			}
 			
 			final UUID uuid = player.getUniqueId();
@@ -144,14 +152,17 @@ public class PBTeamManager {
 						teamsByOnlinePlayer.put(Bukkit.getPlayer(uuid), team);
 						successCallback.run(true);
 					}
+					return;
 				}
 			});
+			return true;
 		}
 	}
 
-	public void handleLeaveTeam(Player player, PBTeam team, final Callback<Boolean> successCallback) {
+	public boolean handleLeaveTeam(Player player, PBTeam team, final Callback<Boolean> successCallback) {
 		if (!teamsByOnlinePlayer.containsKey(player)) {
 			successCallback.run(false);
+			return false;
 		} else {
 			final UUID uuid = player.getUniqueId();
 			this.teamStorage.leaveTeam(uuid, team, new Runnable() {
@@ -164,10 +175,11 @@ public class PBTeamManager {
 					}
 				}
 			});
+			return true;
 		}
 	}
 	
-	public void handleKickPlayer(Player player, PBTeam team, final Callback<Boolean> successCallback) {
+	public boolean handleKickPlayer(Player player, PBTeam team, final Callback<Boolean> successCallback) {
 		if (!teamsByOnlinePlayer.containsKey(player)) {
 			successCallback.run(false);
 			final UUID uuid = player.getUniqueId();
@@ -181,7 +193,9 @@ public class PBTeamManager {
 					}
 				}
 			});
+			return true;
 		}
+		return false;
 	}
 	
 	public List<PBTeam> getTeamList() {
--- src/com/projectkorra/probending/objects/PBGear.java
+++ src/com/projectkorra/probending/objects/PBGear.java
@@ -5,41 +5,43 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
+import com.projectkorra.probending.enums.TeamColor;
+
 public class PBGear {
 
     private ItemStack helmet, chestplate, leggings, boots;
 
-    public PBGear(Color color) {
+    public PBGear(TeamColor color) {
         this.helmet = getGear(Material.LEATHER_HELMET, color);
         this.chestplate = getGear(Material.LEATHER_CHESTPLATE, color);
         this.leggings = getGear(Material.LEATHER_LEGGINGS, color);
         this.boots = getGear(Material.LEATHER_BOOTS, color);
     }
 
-    public PBGear(Color helmet, Color chestplate, Color leggins, Color boots) {
+    public PBGear(TeamColor helmet, TeamColor chestplate, TeamColor leggins, TeamColor boots) {
         this.helmet = getGear(Material.LEATHER_HELMET, helmet);
         this.chestplate = getGear(Material.LEATHER_CHESTPLATE, chestplate);
         this.leggings = getGear(Material.LEATHER_LEGGINGS, leggins);
         this.boots = getGear(Material.LEATHER_BOOTS, boots);
     }
 
-    public void setHelmetColor(Color color) {
+    public void setHelmetTeamColor(TeamColor color) {
         helmet = getGear(Material.LEATHER_HELMET, color);
     }
 
-    public void setChestplateColor(Color color) {
+    public void setChestplateTeamColor(TeamColor color) {
         chestplate = getGear(Material.LEATHER_CHESTPLATE, color);
     }
 
-    public void setLeggingsColor(Color color) {
+    public void setLeggingsTeamColor(TeamColor color) {
         leggings = getGear(Material.LEATHER_LEGGINGS, color);
     }
 
-    public void setBootsColor(Color color) {
+    public void setBootsTeamColor(TeamColor color) {
         boots = getGear(Material.LEATHER_BOOTS, color);
     }
 
-    private ItemStack getGear(Material material, Color color) {
+    private ItemStack getGear(Material material, TeamColor color) {
         ItemStack item;
         LeatherArmorMeta meta;
         if (!(material.equals(Material.LEATHER_BOOTS) || material.equals(Material.LEATHER_LEGGINGS) || material.equals(Material.LEATHER_CHESTPLATE) || material.equals(Material.LEATHER_HELMET))) {
@@ -47,7 +49,7 @@ public class PBGear {
         }
         item = new ItemStack(material);
         meta = (LeatherArmorMeta) item.getItemMeta();
-        meta.setColor(color);
+        meta.setColor(Color.fromRGB(color.getRValue(), color.getGValue(), color.getBValue()));
         item.setItemMeta(meta);
         return item;
     }
